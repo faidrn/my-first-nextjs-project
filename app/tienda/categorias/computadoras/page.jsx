730155e8402c09bcf25afd7e2aa127e479d6d075
Fredy Izquierdo <fredyizquierdo.dev@gmail.com>
2025-12-23 23:24:49 +0100

    Aprendiendo sobre enrutamiento con Next.js

---
+++ app/tienda/categorias/computadoras/page.jsx
@@ -0,0 +1,5 @@
+function Computadoras() {
+    return <h1>Computadoras</h1>
+};
+
+export default Computadoras;
---
+++ app/tienda/categorias/page.jsx
@@ -0,0 +1,5 @@
+const Categorias = () => {
+    return <h1>Categorias de Productos</h1>
+};
+
+export default Categorias;
