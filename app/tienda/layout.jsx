496c8328c8ecf6dc269d56e9e6b7abf9e4b5c6ec
Fredy Izquierdo <fredyizquierdo.dev@gmail.com>
2025-12-25 15:49:11 +0100

    Agregando layout en las diferentes secciones del proyecto

---
+++ app/tienda/layout.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function TiendaLayout({ children }) {
+    return <>
+        <nav>
+            <h3>Sección Tienda</h3>
+
+            <ul>
+                <li><Link href="/tienda/categorias">Categorías</Link></li>
+                <li><Link href="/tienda/categorias/computadoras">Computadoras</Link></li>
+            </ul>
+        </nav>
+        {children}
+    </>;
+}
