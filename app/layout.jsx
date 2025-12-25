ad89f33498f59b21c3d76586f7cbc1ba78bb4bae
Fredy Izquierdo <fredyizquierdo.dev@gmail.com>
2025-12-25 17:45:13 +0100

    Agregando fuentes al proyecto

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar";
+import { Roboto } from 'next/font/google';
 
 export const metadata = {
     title: 'Mi tienda con Next.js',
@@ -6,10 +7,17 @@ export const metadata = {
     keywords: 'tienda, next.js, ecommerce, productos',
 }
 
+const roboto = Roboto({
+    subsets: ['latin'],
+    weight: ['300', '400', '500', '700'],
+    style: ['normal', 'italic'],
+    variable: '--font-roboto',
+});
+
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body>
+            <body className={roboto.className}>
                 <Navbar />
                 {children}
             </body>
