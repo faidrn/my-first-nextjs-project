0ac509e04550d49f24e0c127c1fd50bd36a517bd
Fredy Izquierdo <fredyizquierdo.dev@gmail.com>
2025-12-28 15:38:01 +0100

    Agregando route handlers al proyecto y obteniendo datos externos con el método GET

---
+++ src/app/api/users/route.js
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    // aqui se puede:
+    // - obtener datos de una base de datos
+    // - llamar a una API externa
+    // - extraer parametros
+    // - comunicarme con otros servicios o con otro backend
+    // - realizar cualquier otra lógica necesaria
+    // luego devolver una respuesta JSON
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return NextResponse.json(data);
+}
+
+export function POST(request) {
+    return NextResponse.json({
+        message: "Creando datos!"
+    });
+}
+
+export function PUT(request) {
+    return NextResponse.json({
+        message: "Actualizando datos!"
+    });
+}
+
+export function DELETE(request) {
+    return NextResponse.json({
+        message: "Eliminando datos!"
+    });
+}
