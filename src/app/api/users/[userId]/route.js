a289eae919dca562bd8d0268c24a63c5d2309e02
Fredy Izquierdo <fredyizquierdo.dev@gmail.com>
2025-12-28 16:46:54 +0100

    Obteniendo parametros pasados por URL

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -2,6 +2,11 @@ import { NextResponse } from "next/server";
 
 export async function GET(request, { params }) {
     const { userId } = await params;
+    // http://localhost:3000/api/users/1?nombre=L&apellido=S
+    const { searchParams } = new URL(request.url);
+    console.log("searchParams:", searchParams);
+    console.log(searchParams.get("nombre"));
+    console.log(searchParams.get("apellido"));
     
     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
     const data = await res.json();
